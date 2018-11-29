c491f0af522d0d41bcabdbb9fb5d477497b884a7
Matthawk67 <mdhawk15@gmail.com>
2018-11-29 02:36:01 -0800

    so many issues with git and pycharm

--- web2py/applications/HawkinsPhotography/controllers/default.py
+++ web2py/applications/HawkinsPhotography/controllers/default.py
@@ -17,7 +17,10 @@ def index():
     if you need a simple wiki simply replace the two lines below with:
     return auth.wiki()
     """
-    #response.flash = T("Welcome "+str(auth.user.first_name) + "!")
+    if auth.user.first_name == 'Admin':
+        response.flash = T("Welcome back Matthew!")
+    else:
+        response.flash = T("Welcome "+str(auth.user.first_name) + "!")
     return dict(message=T('Welcome to web2py!'))
 
 
